603c5d3c08d99fe333bdac49d86e2bfe95bcf656
Gustavo Oliveira <gustavo.oliveira.devbr@gmail.com>
2026-04-30 18:48:06 -0300

    add exercise

---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String name = "Gustavo";
+        String address = "Rua Itapeva N 71";
+        double salary = 2280.99;
+        String dateReceived = "29/06";
+        String report = "Eu "+name+", morando no endereço "+address+", confirmo que recebi o salário de "+salary+", na data "+dateReceived;
+        System.out.println(report);
+    }
+}
