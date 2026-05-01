0a7dab3cd4567216e9d2c3a0d89e0d791d315d58
Gustavo Oliveira <gustavo.oliveira.devbr@gmail.com>
2026-04-30 21:20:35 -0300

    update ex 5

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -2,6 +2,24 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula05EstruturasCondicionais {
     public static void main(String[] args) {
+        int age = 15;
+        boolean isAuthorizedPurchaseDrinks = age >= 18;
+        if (age >= 18) {
+            System.out.println("authorized to purchase alcoholic beverages");
+        }
 
+        if (age < 18 ) {
+            System.out.println("Not permitted to purchase alcoholic beverages");
+        }
+
+        // Fiz de uma maneira simples com o que já tinha estudado de lógica e abaixo a mais complexa do prof, ambas funcionam para essa tarefa simples
+
+        if (isAuthorizedPurchaseDrinks){
+            System.out.println("authorized to purchase alcoholic beverages");
+        }
+
+        if (!isAuthorizedPurchaseDrinks){
+            System.out.println("Not permitted to purchase alcoholic beverages");
+        }
     }
 }
