0c443418301ec68ad6cdeefd6620388aa3941753
Gustavo Oliveira <gustavo.oliveira.devbr@gmail.com>
2026-05-01 09:53:30 -0300

    Add lesson 5/4

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais04.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais04 {
+    public static void main(String[] args) {
+        double annualSalary = 65000;
+        double firstTrack = 9.70 / 100;
+        double secondTrack = 37.35 / 100;
+        double thirdTrack = 49.50 / 100;
+        double taxAmount;
+        if (annualSalary <= 34712) {
+            annualSalary = annualSalary * firstTrack;
+        }else if (annualSalary >= 34713 && annualSalary <= 68507) {
+            annualSalary = annualSalary * secondTrack;
+        }else {
+            annualSalary = annualSalary * thirdTrack;
+        }
+        System.out.println("Total annual tax amount: "+annualSalary);
+    }
+}
