19ab36564d8d3a2581210e9431203d6ddb258078
Gustavo Oliveira <gustavo.oliveira.devbr@gmail.com>
2026-05-01 10:34:50 -0300

    update ex

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais04.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais04.java
@@ -2,7 +2,7 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula05EstruturasCondicionais04 {
     public static void main(String[] args) {
-        double annualSalary = 65000;
+        double annualSalary = 70000;
         double firstTrack = 9.70 / 100;
         double secondTrack = 37.35 / 100;
         double thirdTrack = 49.50 / 100;
