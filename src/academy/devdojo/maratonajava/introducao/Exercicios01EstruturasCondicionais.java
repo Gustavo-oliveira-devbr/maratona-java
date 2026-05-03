4cb8a0b3de89818af93f7f8198fe1e9eaec310d5
Gustavo Oliveira <gustavo.oliveira.devbr@gmail.com>
2026-05-03 20:54:27 -0300

    exercise 01 EC

---
+++ src/academy/devdojo/maratonajava/introducao/Exercicios01EstruturasCondicionais.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Exercicios01EstruturasCondicionais {
+    public static void main(String[] args) {
+        int numero = -5;
+
+        if (numero >= 0) {
+            System.out.println("O número é positivo.");
+        } else {
+            System.out.println("O número é negativo.");
+        }
+    }
+}
